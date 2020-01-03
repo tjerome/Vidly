8762f24975d942760dc295441b9f31a2b443e0d1
tyler <tylerjerome93@gmail.com>
2020-01-02 21:32:51 -0500

    Created API for customers

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -27,21 +27,21 @@
             context.Customers.AddOrUpdate(new Customer() { Id = 1, Name = "John Smith", isSubscribedToNewsletter = false, MembershipTypeId = 1, Birthday = new DateTime(2003, 5, 23)},
                 new Customer() { Id = 2, Name = "Mary Williams", isSubscribedToNewsletter = false, MembershipTypeId = 2, Birthday = new DateTime(1993, 12, 24)});
 
-            context.Movies.AddOrUpdate(new Movie() { Name = "Shrek", GenreId = 5, ReleaseDate = new DateTime(2001, 4, 22), DateAdded = DateTime.Now, Stock = 6 },
-                new Movie() { Name = "Frozen 2", GenreId = 5, ReleaseDate = new DateTime(2019, 11, 22), DateAdded = DateTime.Now, Stock = 11 },
-            new Movie() { Name = "Star Wars: The Last Jedi", GenreId = 1, ReleaseDate = new DateTime(2017, 12, 15), DateAdded = DateTime.Now, Stock = 9 },
-            new Movie() { Name = "Balls Of Fury", GenreId = 2, ReleaseDate = new DateTime(2007, 10, 29), DateAdded = DateTime.Now, Stock = 2},
-            new Movie() { Name = "Mamma Mia!", GenreId = 6, ReleaseDate = new DateTime(2008, 6, 30), DateAdded = DateTime.Now, Stock = 3 });
+            //context.Movies.AddOrUpdate(new Movie() { Name = "Shrek", GenreId = 5, ReleaseDate = new DateTime(2001, 4, 22), DateAdded = DateTime.Now, Stock = 6 },
+            //    new Movie() { Name = "Frozen 2", GenreId = 5, ReleaseDate = new DateTime(2019, 11, 22), DateAdded = DateTime.Now, Stock = 11 },
+            //new Movie() { Name = "Star Wars: The Last Jedi", GenreId = 1, ReleaseDate = new DateTime(2017, 12, 15), DateAdded = DateTime.Now, Stock = 9 },
+            //new Movie() { Name = "Balls Of Fury", GenreId = 2, ReleaseDate = new DateTime(2007, 10, 29), DateAdded = DateTime.Now, Stock = 2},
+            //new Movie() { Name = "Mamma Mia!", GenreId = 6, ReleaseDate = new DateTime(2008, 6, 30), DateAdded = DateTime.Now, Stock = 3 });
 
-            context.Genres.AddOrUpdate(new Genre() { Name = "Action" },
-                new Genre { Name = "Comedy" },
-                new Genre { Name = "Drama" },
-                new Genre { Name = "Romance" },
-                new Genre { Name = "Kids" },
-                new Genre { Name = "Musical" },
-                new Genre { Name = "Documentary" },
-                new Genre { Name = "Anime" },
-                new Genre { Name = "Horror" });
+            context.Genres.AddOrUpdate(new Genre() { Id = 1, Name = "Action" },
+                new Genre { Id = 2, Name = "Comedy" },
+                new Genre { Id = 3, Name = "Drama" },
+                new Genre { Id = 4, Name = "Romance" },
+                new Genre { Id = 5, Name = "Kids" },
+                new Genre { Id = 6, Name = "Musical" },
+                new Genre { Id = 7, Name = "Documentary" },
+                new Genre { Id = 8, Name = "Anime" },
+                new Genre { Id = 9, Name = "Horror" });
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,6 +10,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Release Date")]
@@ -18,8 +20,10 @@ namespace Vidly.Models
         [Display(Name = "Date Added")]
         public DateTime DateAdded { get; set; }
 
+        [Range(1,20)]
         public int Stock { get; set; }
 
+        [Required]
         [Display(Name = "Genre")]
         public int GenreId { get; set; }
 
